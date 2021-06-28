a0ba266b469fd52102d916e4fc51ad08e1cd20f2
Helena Paixão <hp.helenapaixao@gmail.com>
2021-06-27 23:46:55 -0300

    header almost done

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
-
-
+import Avatar from '../../assets/images/Avatar.png';
+import * as S from './styles'
 const Header: React.FC = () => {
   return(
-    <h1>Header</h1>
+    <>
+    <S.Container>
+      <S.Content>
+      <h1>Header</h1>
+      </S.Content>
+      <S.Avatar>
+      <img src={Avatar} />
+      </S.Avatar> 
+    </S.Container>
+    </>
   )
 }
 
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,8 +1,43 @@
 import styled from 'styled-components';
 
 
+
 export const Container = styled.div`
+  width: 100%;
+  height: 87px;
+  background:  #F6F6F6;
+  padding: 25px;
+  display: flex;
+  justify-content: space-around;
+  @media all and (max-width: 769px) {
+    background: #fff;
+    h3 {
+      display: none;
+    }
+  }
+  h3 {
+    font-size: 26px;
+    font-family: Arial;
+  }
+`;
+
+export const Content = styled.div`
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+
+`;
 
 
-
+export const Avatar = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  img {
+    margin-right: 10%;
+    @media all and (max-width: 300px) {
+      margin-right: 0px;
+    }
+  }
 `;
